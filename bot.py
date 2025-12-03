91b8d10aae1f4e88b0ec232541893b86bb70cd58
B1ade3 <86893706+B1ade3@users.noreply.github.com>
2025-12-04 00:14:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     print("Bot started successfully!")
-    app.run_polling()
+app.run_polling(drop_pending_updates=True)
 
 if __name__ == '__main__':
     main()
